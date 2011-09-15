f31b25ac5b4fa66b95c65b0f9ba444c8d91d4ef3
Monish Dombla <monish.dombla@semanticbits.com>
2011-09-15 19:02:27 +0000

    ESD-3183 : store permission sql for tests.
    SVN-Revision: 17989

---
+++ xds-authz-handlers/src/main/resources/xds_store_permission.sql
@@ -0,0 +1,3 @@
+INSERT INTO tbl_resource(resource_type, entityid) VALUES ('operation-store',-101);
+INSERT INTO tbl_subject(entityid, dn) VALUES (-101, 'CN=XDSb_REG_MISYS.ihe.net, OU=Connectathon, O=IHE International, L=Chicago, ST=Illinois, C=US');
+INSERT INTO tbl_resource_tbl_subject(tbl_resource_entityid, subjects_entityid) VALUES (-101, -101);
