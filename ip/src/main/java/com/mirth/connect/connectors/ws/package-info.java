4cd0121abe1e47fad1ba3a79860b3bedf3402690
Manav Kher <manav.kher@semanticbits.com>
2011-09-26 20:35:00 +0000

    ESD-3217 update MC installation guide  with instructions on using Postgres
    SVN-Revision: 18126

--- ip/src/main/java/com/mirth/connect/connectors/ws/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://ws.connectors.connect.mirth.com/")
-package com.mirth.connect.connectors.ws;
--- ip/startup.sh
+++
@@ -1,3 +0,0 @@
-export JAVA_OPTS="-Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false"
-
-"$JAVA_HOME/bin/java" $JAVA_OPTS -jar target/ipf-1.0-SNAPSHOT.jar gov.nih.nci.cacis.SampleServer
