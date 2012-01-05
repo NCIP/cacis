2729c931ecf7ab8b0ee5224c06db58a345e349bf
Brian Pickeral <bpickeral@5amsolutions.com>
2012-01-05 16:26:49 +0000

    CACIS-197 Unable to run the caCIS_FREE_TEXT_INDICES.sql as mentioned in the Deployment Guide.
    SVN-Revision: 18594

--- cdw/src/main/resources/procedures/caCIS_FREE_TEXT_INDICES.sql
+++ cdw/src/main/resources/procedures/caCIS_FREE_TEXT_INDICES.sql
@@ -1,29 +1,19 @@
-DDB.DBA.RDF_OBJ_FT_RULE_ADD (null, 'urn:hl7-org:v3/owl/DEFN/UV/DT/1.0#ED.value', 'Index ED value')
-/
+DB.DBA.RDF_OBJ_FT_RULE_ADD (null, 'urn:hl7-org:v3/owl/DEFN/UV/DT/1.0#ED.value', 'Index ED value');
 
-DB.DBA.RDF_OBJ_FT_RULE_ADD (null, 'urn:hl7-org:v3/owl/DEFN/UV/DT/1.0#CD.code', 'CD.code')
-/
+DB.DBA.RDF_OBJ_FT_RULE_ADD (null, 'urn:hl7-org:v3/owl/DEFN/UV/DT/1.0#CD.code', 'CD.code');
 
-DB.DBA.RDF_OBJ_FT_RULE_ADD (null, 'urn:hl7-org:v3/owl/DEFN/UV/DT/1.0#CD.codeSystem', 'CD.codeSystem')
-/
+DB.DBA.RDF_OBJ_FT_RULE_ADD (null, 'urn:hl7-org:v3/owl/DEFN/UV/DT/1.0#CD.codeSystem', 'CD.codeSystem');
 
-DB.DBA.RDF_OBJ_FT_RULE_ADD (null, 'urn:hl7-org:v3/owl/DEFN/UV/DT/1.0#CD.codeSystemName', 'CD.codeSystemName')
-/
+DB.DBA.RDF_OBJ_FT_RULE_ADD (null, 'urn:hl7-org:v3/owl/DEFN/UV/DT/1.0#CD.codeSystemName', 'CD.codeSystemName');
 
-DB.DBA.RDF_OBJ_FT_RULE_ADD (null, 'urn:hl7-org:v3/owl/DEFN/UV/DT/1.0#CD.codeSystemVersion', 'CD.codeSystemVersion')
-/
+DB.DBA.RDF_OBJ_FT_RULE_ADD (null, 'urn:hl7-org:v3/owl/DEFN/UV/DT/1.0#CD.codeSystemVersion', 'CD.codeSystemVersion');
 
-DB.DBA.RDF_OBJ_FT_RULE_ADD (null, 'urn:hl7-org:v3/owl/DEFN/UV/DT/1.0#CD.displayName', 'CD.displayName')
-/
+DB.DBA.RDF_OBJ_FT_RULE_ADD (null, 'urn:hl7-org:v3/owl/DEFN/UV/DT/1.0#CD.displayName', 'CD.displayName');
 
-DB.DBA.RDF_OBJ_FT_RULE_ADD (null, 'urn:hl7-org:v3/owl/DEFN/UV/DT/1.0#II.root', 'II.root')
-/
+DB.DBA.RDF_OBJ_FT_RULE_ADD (null, 'urn:hl7-org:v3/owl/DEFN/UV/DT/1.0#II.root', 'II.root');
 
-DB.DBA.RDF_OBJ_FT_RULE_ADD (null, 'urn:hl7-org:v3/owl/DEFN/UV/DT/1.0#II.extension', 'II.extension')
-/
+DB.DBA.RDF_OBJ_FT_RULE_ADD (null, 'urn:hl7-org:v3/owl/DEFN/UV/DT/1.0#II.extension', 'II.extension');
 
-DB.DBA.RDF_OBJ_FT_RULE_ADD (null, 'urn:hl7-org:v3/owl/DEFN/UV/DT/1.0#TS.value', 'TS.value')
-/
+DB.DBA.RDF_OBJ_FT_RULE_ADD (null, 'urn:hl7-org:v3/owl/DEFN/UV/DT/1.0#TS.value', 'TS.value');
 
-DB.DBA.RDF_OBJ_FT_RULE_ADD (null, 'urn:hl7-org:v3/owl/DEFN/UV/DT/1.0#URL.address', 'URL.address')
-/
+DB.DBA.RDF_OBJ_FT_RULE_ADD (null, 'urn:hl7-org:v3/owl/DEFN/UV/DT/1.0#URL.address', 'URL.address');
