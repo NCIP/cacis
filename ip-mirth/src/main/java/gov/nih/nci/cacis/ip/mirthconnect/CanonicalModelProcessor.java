756c30860b41ee2515d7234ab69eefc91ea5cd28
Ajay Nalamala <Ajay.Nalamala@ekagrasoft.com>
2012-04-09 16:22:12 +0000

    Few changes made to check additional strings for determining the error response.
    SVN-Revision: 18672

--- ip-mirth/src/main/java/gov/nih/nci/cacis/ip/mirthconnect/CanonicalModelProcessor.java
+++ ip-mirth/src/main/java/gov/nih/nci/cacis/ip/mirthconnect/CanonicalModelProcessor.java
@@ -4,13 +4,13 @@
  * NCI employees and 5AM Solutions Inc, SemanticBits LLC, and AgileX Technologies, Inc (collectively 'SubContractors').
  * To the extent government employees are authors, any rights in such works shall be subject to Title 17 of the United
  * States Code, section 105.
- *
+ * 
  * This caEHR Software License (the License) is between NCI and You. You (or Your) shall mean a person or an entity, and
  * all other entities that control, are controlled by, or are under common control with the entity. Control for purposes
  * of this definition means (i) the direct or indirect power to cause the direction or management of such entity,
  * whether by contract or otherwise, or (ii) ownership of fifty percent (50%) or more of the outstanding shares, or
  * (iii) beneficial ownership of such entity.
- *
+ * 
  * This License is granted provided that You agree to the conditions described below. NCI grants You a non-exclusive,
  * worldwide, perpetual, fully-paid-up, no-charge, irrevocable, transferable and royalty-free right and license in its
  * rights in the caEHR Software to (i) use, install, access, operate, execute, copy, modify, translate, market, publicly
@@ -20,22 +20,22 @@
  * third parties. For sake of clarity, and not by way of limitation, NCI shall have no right of accounting or right of
  * payment from You or Your sub-licensees for the rights granted under this License. This License is granted at no
  * charge to You.
- *
+ * 
  * Your redistributions of the source code for the Software must retain the above copyright notice, this list of
  * conditions and the disclaimer and limitation of liability of Article 6, below. Your redistributions in object code
  * form must reproduce the above copyright notice, this list of conditions and the disclaimer of Article 6 in the
  * documentation and/or other materials provided with the distribution, if any.
- *
+ * 
  * Your end-user documentation included with the redistribution, if any, must include the following acknowledgment: This
  * product includes software developed by the National Cancer Institute and SubContractor parties. If You do not include
  * such end-user documentation, You shall include this acknowledgment in the Software itself, wherever such third-party
  * acknowledgments normally appear.
- *
+ * 
  * You may not use the names "The National Cancer Institute", "NCI", or any SubContractor party to endorse or promote
  * products derived from this Software. This License does not authorize You to use any trademarks, service marks, trade
  * names, logos or product names of either NCI or any of the subcontracted parties, except as required to comply with
  * the terms of this License.
- *
+ * 
  * For sake of clarity, and not by way of limitation, You may incorporate this Software into Your proprietary programs
  * and into any third party proprietary programs. However, if You incorporate the Software into third party proprietary
  * programs, You agree that You are solely responsible for obtaining any permission from such third parties required to
@@ -44,12 +44,12 @@
  * before incorporating the Software into such third party proprietary software programs. In the event that You fail to
  * obtain such permissions, You agree to indemnify NCI for any claims against NCI by such third parties, except to the
  * extent prohibited by law, resulting from Your failure to obtain such permissions.
- *
+ * 
  * For sake of clarity, and not by way of limitation, You may add Your own copyright statement to Your modifications and
  * to the derivative works, and You may provide additional or different license terms and conditions in Your sublicenses
  * of modifications of the Software, or any derivative works of the Software as a whole, provided Your use,
  * reproduction, and distribution of the Work otherwise complies with the conditions stated in this License.
- *
+ * 
  * THIS SOFTWARE IS PROVIDED "AS IS," AND ANY EXPRESSED OR IMPLIED WARRANTIES, (INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY, NON-INFRINGEMENT AND FITNESS FOR A PARTICULAR PURPOSE) ARE DISCLAIMED. IN NO
  * EVENT SHALL THE NATIONAL CANCER INSTITUTE, ANY OF ITS SUBCONTRACTED PARTIES OR THEIR AFFILIATES BE LIABLE FOR ANY
@@ -76,22 +76,22 @@ import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 
+import org.apache.commons.lang.StringUtils;
+
 import com.mirth.connect.connectors.ws.AcceptMessage;
 import com.mirth.connect.connectors.ws.WebServiceMessageReceiver;
 
 /**
  * @author kherm manav.kher@semanticbits.com
  */
-@WebService(serviceName = "CanonicalModelProcessor", portName = "CanonicalModelProcessor_Port_Soap11",
-        targetNamespace = "http://cacis.nci.nih.gov",
-        endpointInterface = "gov.nih.nci.cacis.CanonicalModelProcessorPortType")
+@WebService(serviceName = "CanonicalModelProcessor", portName = "CanonicalModelProcessor_Port_Soap11", targetNamespace = "http://cacis.nci.nih.gov", endpointInterface = "gov.nih.nci.cacis.CanonicalModelProcessorPortType")
 public class CanonicalModelProcessor extends AcceptMessage {
 
     private static final String CACIS_NS = "http://cacis.nci.nih.gov";
 
     /**
      * Constructor
-     *
+     * 
      * @param webServiceMessageReceiver Mirth/Mule webServiceMessageReceiver
      */
     public CanonicalModelProcessor(WebServiceMessageReceiver webServiceMessageReceiver) {
@@ -100,7 +100,7 @@ public class CanonicalModelProcessor extends AcceptMessage {
 
     /**
      * Method accepts canonical data for processing
-     *
+     * 
      * @param request CaCISRequest
      * @return response CaCISResponse
      * @throws AcceptCanonicalFault Fault
@@ -113,7 +113,6 @@ public class CanonicalModelProcessor extends AcceptMessage {
             throws AcceptCanonicalFault {
 
         final CaCISResponse response = new CaCISResponse();
-        response.setStatus(ResponseStatusType.SUCCESS);
 
         final StringWriter sw = new StringWriter();
         try {
@@ -122,14 +121,18 @@ public class CanonicalModelProcessor extends AcceptMessage {
 
             final PrintWriter pw = new PrintWriter(sw);
             m.marshal(request, pw);
+            response.setStatus(ResponseStatusType.SUCCESS);
         } catch (JAXBException jaxE) {
             throw new AcceptCanonicalFault("Error Marshalling object", jaxE);
         }
 
         try {
-            final String mcResponse = webServiceMessageReceiver.processData(sw.toString());
+            String mcResponse = webServiceMessageReceiver.processData(sw.toString());
 
-            if (mcResponse != null && mcResponse.indexOf("Error") > -1) {                
+            if (mcResponse != null
+                    && (mcResponse.indexOf("Error") > -1 || mcResponse.indexOf("Exception") > -1
+                            || mcResponse.indexOf("ERROR") > -1 || mcResponse.indexOf("error") > -1)) {
+                mcResponse = StringUtils.remove(mcResponse, "SUCCESS:");
                 throw new AcceptCanonicalFault("Error processing Data from Source System: " + mcResponse);
             }
             response.setStatus(ResponseStatusType.SUCCESS);
