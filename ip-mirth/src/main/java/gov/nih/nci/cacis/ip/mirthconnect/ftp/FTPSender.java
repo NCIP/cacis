809b52008738728a2350d0021808e9d90e4f967d
Ajay Nalamala <Ajay.Nalamala@ekagrasoft.com>
2012-06-28 18:19:49 +0000

    Added a check for the protocol to be 'sftp' or 'ftps'.
    SVN-Revision: 18743

--- ip-mirth/src/main/java/gov/nih/nci/cacis/ip/mirthconnect/ftp/FTPSender.java
+++ ip-mirth/src/main/java/gov/nih/nci/cacis/ip/mirthconnect/ftp/FTPSender.java
@@ -4,13 +4,13 @@
  * NCI employees and 5AM Solutions Inc, SemanticBits LLC, and AgileX Technologies, Inc (collectively 'SubContractors').
  * To the extent government employees are authors, any rights in such works shall be subject to Title 17 of the United
  * States Code, section 105.
- *
+ * 
  * This caEHR Software License (the License) is between NCI and You. You (or Your) shall mean a person or an entity, and
  * all other entities that control, are controlled by, or are under common control with the entity. Control for purposes
  * of this definition means (i) the direct or indirect power to cause the direction or management of such entity,
  * whether by contract or otherwise, or (ii) ownership of fifty percent (50%) or more of the outstanding shares, or
  * (iii) beneficial ownership of such entity.
- *
+ * 
  * This License is granted provided that You agree to the conditions described below. NCI grants You a non-exclusive,
  * worldwide, perpetual, fully-paid-up, no-charge, irrevocable, transferable and royalty-free right and license in its
  * rights in the caEHR Software to (i) use, install, access, operate, execute, copy, modify, translate, market, publicly
@@ -20,22 +20,22 @@
  * third parties. For sake of clarity, and not by way of limitation, NCI shall have no right of accounting or right of
  * payment from You or Your sub-licensees for the rights granted under this License. This License is granted at no
  * charge to You.
- *
+ * 
  * Your redistributions of the source code for the Software must retain the above copyright notice, this list of
  * conditions and the disclaimer and limitation of liability of Article 6, below. Your redistributions in object code
  * form must reproduce the above copyright notice, this list of conditions and the disclaimer of Article 6 in the
  * documentation and/or other materials provided with the distribution, if any.
- *
+ * 
  * Your end-user documentation included with the redistribution, if any, must include the following acknowledgment: This
  * product includes software developed by the National Cancer Institute and SubContractor parties. If You do not include
  * such end-user documentation, You shall include this acknowledgment in the Software itself, wherever such third-party
  * acknowledgments normally appear.
- *
+ * 
  * You may not use the names "The National Cancer Institute", "NCI", or any SubContractor party to endorse or promote
  * products derived from this Software. This License does not authorize You to use any trademarks, service marks, trade
  * names, logos or product names of either NCI or any of the subcontracted parties, except as required to comply with
  * the terms of this License.
- *
+ * 
  * For sake of clarity, and not by way of limitation, You may incorporate this Software into Your proprietary programs
  * and into any third party proprietary programs. However, if You incorporate the Software into third party proprietary
  * programs, You agree that You are solely responsible for obtaining any permission from such third parties required to
@@ -44,12 +44,12 @@
  * before incorporating the Software into such third party proprietary software programs. In the event that You fail to
  * obtain such permissions, You agree to indemnify NCI for any claims against NCI by such third parties, except to the
  * extent prohibited by law, resulting from Your failure to obtain such permissions.
- *
+ * 
  * For sake of clarity, and not by way of limitation, You may add Your own copyright statement to Your modifications and
  * to the derivative works, and You may provide additional or different license terms and conditions in Your sublicenses
  * of modifications of the Software, or any derivative works of the Software as a whole, provided Your use,
  * reproduction, and distribution of the Work otherwise complies with the conditions stated in this License.
- *
+ * 
  * THIS SOFTWARE IS PROVIDED "AS IS," AND ANY EXPRESSED OR IMPLIED WARRANTIES, (INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY, NON-INFRINGEMENT AND FITNESS FOR A PARTICULAR PURPOSE) ARE DISCLAIMED. IN NO
  * EVENT SHALL THE NATIONAL CANCER INSTITUTE, ANY OF ITS SUBCONTRACTED PARTIES OR THEIR AFFILIATES BE LIABLE FOR ANY
@@ -79,16 +79,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 public class FTPSender {
 
     @Autowired
-    private FTPSSender ftpsSender;    
-    
+    private FTPSSender ftpsSender;
+
     @Autowired
-    private SFTPSender sftpSender;    
+    private SFTPSender sftpSender;
 
     @Autowired
     private FTPMapping ftpMapping;
 
     /**
      * Sends Document to FTPS Server.
+     * 
      * @param file Input Stream.
      * @param ftpAddress the ftp address in which to store the file.
      * @param extension File extension
@@ -104,16 +105,17 @@ public class FTPSender {
             UnrecoverableKeyException {
         final FTPInfo ftpInfo = ftpMapping.getFTPInfo(ftpAddress);
         if (ftpInfo == null) {
-            throw new ApplicationRuntimeException("No server config exists for address[ " + ftpAddress +" ]");
+            throw new ApplicationRuntimeException("No server config exists for address[ " + ftpAddress + " ]");
         }
 
-        if (ftpInfo.getProtocol() == FTPInfo.FTPS)
-        {
-        	ftpsSender.sendDocument(file, ftpAddress, extension);
-        }
-        else if (ftpInfo.getProtocol() == FTPInfo.SFTP)
-        {
-        	sftpSender.sendDocument(file, ftpAddress, extension);
+        if (ftpInfo.getProtocol() == FTPInfo.FTPS) {
+            ftpsSender.sendDocument(file, ftpAddress, extension);
+        } else if (ftpInfo.getProtocol() == FTPInfo.SFTP) {
+            sftpSender.sendDocument(file, ftpAddress, extension);
+        } else {
+            throw new IOException(
+                    "The FTP protocol entry in the configuration file has to be 'ftps' or 'sftp'. It is currently configured as [ "
+                            + ftpInfo.getProtocol() + " ]");
         }
     }
 
