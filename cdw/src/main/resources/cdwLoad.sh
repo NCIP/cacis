53bf2911326e18f83a3de1b96c2d9a2d05cc2c4c
Ajay Nalamala <Ajay.Nalamala@ekagrasoft.com>
2012-07-16 19:14:51 +0000

    Updated shell script with classpath search.
    SVN-Revision: 18760

--- cdw/src/main/resources/cdwLoad.sh
+++ cdw/src/main/resources/cdwLoad.sh
@@ -1,12 +1,1 @@
-export JAR_HOME=.
-
-for f in $JAR_HOME/*.jar
-do
-JAR_CLASSPATH=$JAR_CLASSPATH:$f
-done
-export JAR_CLASSPATH
-
-#the next line will print the JAR_CLASSPATH to the shell.
-echo the classpath $JAR_CLASSPATH
-
-java -classpath $JAR_CLASSPATH gov.nih.nci.cacis.cdw.CDWLoadJob
+java -classpath classes:'*' gov.nih.nci.cacis.cdw.CDWLoadJob
