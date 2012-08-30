7b4db2c1ddc36391cfe45f0be50f6ddb3b1433cf
Ajay Nalamala <Ajay.Nalamala@ekagrasoft.com>
2012-08-30 14:59:14 +0000

    Updated with JAVA_HOME
    SVN-Revision: 18798

--- cdw/src/main/resources/cdwLoad.sh
+++ cdw/src/main/resources/cdwLoad.sh
@@ -1,2 +1,2 @@
 #java -classpath classes:'*' gov.nih.nci.cacis.cdw.CDWLoadJob
-java -classpath classes:'${cdw.load.job.directory}/*' gov.nih.nci.cacis.cdw.CDWLoadJob
+$JAVA_HOME/bin/java -classpath classes:'${cdw.load.job.directory}/*' gov.nih.nci.cacis.cdw.CDWLoadJob
